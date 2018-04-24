a610ae4ba753e047ce0bf501611dcd6b493ef932
Pascal Isman <pascal@isman.fr>
2018-04-24 12:36:00 +0200

    add a database configuration file

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
 #mongoBD
-print "hello world"
+import pymongo
+import sys
+
+print "Encymon"
+
+#read the database file configuration 
+try: 
+	configDataBase = open("./config.database.txt","r")
+except: 
+	print "I can't open ./config.database.txt"
+	print "Can you verify if it's created !"
+
+#opendatabase connection
+client = pymongo.MongoClient(configDataBase.read())
+db = client.test
+
+#collection = db.test_collection
+
+#print collection
+print "end"
